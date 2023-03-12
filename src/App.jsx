f7ecfa736a66d94e93b57198bcdbc83465ee6e00
a-klimovich <kom9toshka@gmail.com>
2023-03-12 19:07:25 +0100

    add: bubles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Buble from './components/Buble'
 import Container from './components/Container'
 import MainContent from './modules/MainContent'
 import './scss/index.scss'
@@ -15,6 +16,12 @@ const App = () => (
       </main>
 
       <Footer />
+
+      <Buble size="311px" variant="redLight" top="251px" left="-102px" />
+      <Buble size="83px" variant="redBoll" top="83px" left="519px" />
+      <Buble size="259px" variant="purpleLight" top="-154px" left="602px" />
+      <Buble size="60px" variant="purpleBall" top="-2px" left="868px" />
+      <Buble size="28px" variant="redBoll" top="547px" left="851px" />
     </div>
   </Container>
 )
